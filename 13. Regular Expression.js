5eb50f15b43bd925fdc35cd01390dce21aba4ab0
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-22 21:35:56 +0600

    Update 13. Regular Expression.js

--- 13. Regular Expression.js
+++ 13. Regular Expression.js
@@ -10,7 +10,17 @@ document.write("<br>");
 document.write(rel.source);
 
 restr = "Hello World";
+restr = "Again Hello World";
+restr = "AgainHello World";
+restr = "Again Hello World Hello";
 
-let result = rel.exec(restr);
+//exec() Returns result in an array or null
+let reresult = rel.exec(restr);
+//test() - True/False
+let reresultt = rel.test(restr);
+//match() Returns Array or Null
+let reresultm = restr.match(rel);
+//search() Returns index of the first match or -1
+let reresultse = restr.search(rel);
 
-console.log(result);
+console.log(reresultse);
