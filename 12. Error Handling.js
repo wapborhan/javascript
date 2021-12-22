ee108b9ee0d4cb11989b01d046ff53dd25e9c886
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-22 18:09:32 +0600

    Update 12. Error Handling.js

--- 12. Error Handling.js
+++ 12. Error Handling.js
@@ -1,13 +1,22 @@
-document.write("Before Error!");
-document.write("<br>");
+// document.write("Before Error!");
+// document.write("<br>");
+
+// try {
+//   test();
+// } catch (err) {
+//   // document.write(err);
+//   document.write(`Type: ${err.name} "${err.message}"`);
+// } finally {
+//   document.write("<br>" + "I am inside Filany!");
+// }
+// document.write("<br>");
+// document.write("After Error!");
+
+let eb = 24;
 
 try {
-  test();
+  if (eb > 15) throw "Too Big";
+  else if (eb < 5) throw "Too Small";
 } catch (err) {
-  // document.write(err);
-  document.write(`Type: ${err.name} "${err.message}"`);
-} finally {
-  document.write("<br>" + "I am inside Filany!");
+  console.log(err);
 }
-document.write("<br>");
-document.write("After Error!");
