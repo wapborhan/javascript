55f8de2f5c303f24843092812b8559c92acd4f08
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-22 18:55:04 +0600

    Update 13. Regular Expression.js

--- 13. Regular Expression.js
+++ 13. Regular Expression.js
@@ -2,8 +2,15 @@
 
 let rel;
 
-rel = /Hello/;
+rel = /hello/; //case sensitive
+rel = /hello/i; //case insensitive
 
 document.write(rel);
 document.write("<br>");
 document.write(rel.source);
+
+restr = "Hello World";
+
+let result = rel.exec(restr);
+
+console.log(result);
