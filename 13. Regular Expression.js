30c3163205513545e7dda5f973822edefa8fb441
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 12:07:06 +0600

    Update 13. Regular Expression.js

--- 13. Regular Expression.js
+++ 13. Regular Expression.js
@@ -31,39 +31,53 @@
 // document.write("<br>" + reresultse);
 // document.write("<br>" + reresultr);
 
-// 02. Literal and Meta Characters
-//Literal Characters
-rel = /hello/; //case sensitive
-rel = /hello/i; //case insensitive
+// // 02. Literal and Meta Characters
+// //Literal Characters
+// rel = /hello/; //case sensitive
+// rel = /hello/i; //case insensitive
 
-// Meta Characters
-rel = /^hello/i; //^ Must Start With
-rel = /hello$/i; //$ Must End With
-rel = /^hello$/i; //$ Must Start and End With
-rel = /h.llo/i; //. Match Any One Characters
-rel = /h*llo/i; // * 0 to More Characters or No Characters
-rel = /he?llo/i; //? Optional
-// // 03. Character Set using Brackets
-rel = /h[ea]llo/i; //[] Must be one of them inside brackets
-rel = /h[^ea]llo/i; //[^] Anything except inside brackets
-rel = /^[he]llo/i; //^[] Must start with anyone inside brackets
-rel = /[a-z]llo/i; //[a-z] range anyone inside brackets
-rel = /^[A-Z]/; //^[A-Z] Frist Letter Must Be Uppercase
-rel = /[A-Za-z]/; //[A-Za-z] anything in letter
-rel = /[0-9]ello/; //[0-9] Start in digit
-rel = /[0-9][0-9]ello/; //[0-9] Start in Two digit
-rel = /[0-9][0-9][0-9]ello/; //[0-9] Start in Three digit
+// // Meta Characters
+// rel = /^hello/i; //^ Must Start With
+// rel = /hello$/i; //$ Must End With
+// rel = /^hello$/i; //$ Must Start and End With
+// rel = /h.llo/i; //. Match Any One Characters
+// rel = /h*llo/i; // * 0 to More Characters or No Characters
+// rel = /he?llo/i; //? Optional
+// // // 03. Character Set using Brackets
+// rel = /h[ea]llo/i; //[] Must be one of them inside brackets
+// rel = /h[^ea]llo/i; //[^] Anything except inside brackets
+// rel = /^[he]llo/i; //^[] Must start with anyone inside brackets
+// rel = /[a-z]llo/i; //[a-z] range anyone inside brackets
+// rel = /^[A-Z]/; //^[A-Z] Frist Letter Must Be Uppercase
+// rel = /[A-Za-z]/; //[A-Za-z] anything in letter
+// rel = /[0-9]ello/; //[0-9] Start in digit
+// rel = /[0-9][0-9]ello/; //[0-9] Start in Two digit
+// rel = /[0-9][0-9][0-9]ello/; //[0-9] Start in Three digit
 
-restr = "Hello";
+// //braces {} - Quantifier
+// rel = /hel{2}o/; // brackets after letter count 2 check
+// rel = /hel{2,5}o/; // brackets after letter count 2-5 check
+// rel = /hel{2,}o/; // brackets after letter count 2-unlimited check
 
-document.write(rel.exec(restr));
-document.write("<br>");
+// //parentheses () - Grouping
+// rel = /^([0-9]){5}/; // 5 digit start must
 
-function reTest(rel, restr) {
-  if (rel.test(restr)) {
-    document.write(`'${restr}' - Matches : '${rel.source}'`);
-  } else {
-    document.write(`'${restr}' - Doesn't Matches : '${rel.source}'`);
-  }
-}
-reTest(rel, restr);
+// //bangladeshi phone number
+// rel = /^01([0-9]){9}$/;
+// rel = /^\+8801([0-9]){9}$/;
+
+// restr = "+8801620557840";
+
+// // document.write(rel.exec(restr));
+// // document.write("<br>");
+
+// function reTest(rel, restr) {
+//   if (rel.test(restr)) {
+//     document.write(`'${restr}' - Matches : '${rel.source}'`);
+//   } else {
+//     document.write(`'${restr}' - Doesn't Matches : '${rel.source}'`);
+//   }
+// }
+// reTest(rel, restr);
+
+// // 04. Shorthand Characters and Assertions
