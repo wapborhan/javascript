8b2fe96a6cccc0e0ad8cfdc348f1c7bd9c969244
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 19:37:09 +0600

    update

--- 15. AJAX.js
+++ 15. AJAX.js
@@ -1,3 +1,8 @@
 // Asynchronous Programming
 // AJAX = Asynchronous Javascript and XML
 // Get Data without loading the page
+document.getElementById("get_data").addEventListener("click", loadData);
+
+function loadData() {
+  console.log("Button Clicked");
+}
