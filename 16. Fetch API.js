8586584e6400f4d2cff9a816039078247f7bb0d0
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 23:25:57 +0600

    edit

--- 16. Fetch API.js
+++ 16. Fetch API.js
@@ -25,32 +25,38 @@ let persons = [
 
 //02. JavaScript Promises
 
-function createPerson(person) {
-  let prom = new Promise(function (resolve, reject) {
-    persons.push(person);
-    let error = false;
+// function createPerson(person) {
+//   let prom = new Promise(function (resolve, reject) {
+//     persons.push(person);
+//     let error = false;
 
-    if (!error) {
-      resolve();
-    } else {
-      reject("Error: Something Wrong!");
-    }
-  });
-  return prom;
-}
+//     if (!error) {
+//       resolve();
+//     } else {
+//       reject("Error: Something Wrong!");
+//     }
+//   });
+//   return prom;
+// }
 
-function getPerson() {
-  setTimeout(function () {
-    let output = "";
-    persons.forEach(function (person) {
-      output += `<li>${person.fristName} ${person.lastName}</li>`;
-    });
-    document.getElementById("output").innerHTML = output;
-  }, 500);
-}
+// function getPerson() {
+//   setTimeout(function () {
+//     let output = "";
+//     persons.forEach(function (person) {
+//       output += `<li>${person.fristName} ${person.lastName}</li>`;
+//     });
+//     document.getElementById("output").innerHTML = output;
+//   }, 500);
+// }
 
-createPerson({ fristName: "Rony", lastName: "Ali" })
-  .then(getPerson)
-  .catch(function (err) {
-    document.write(err);
-  });
+// createPerson({ fristName: "Rony", lastName: "Ali" })
+//   .then(getPerson)
+//   .catch(function (err) {
+//     document.write(err);
+//   });
+
+//03. Fetch API
+
+document.getElementById("getfetch_data").addEventListener("click", fetchData);
+
+function fetchData() {}
