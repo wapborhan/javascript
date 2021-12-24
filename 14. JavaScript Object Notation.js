c355d3ff6a8fc3a1002f24e7bc64ac02641a95e0
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 15:07:34 +0600

    Update 14. JavaScript Object Notation.js

---
+++ 14. JavaScript Object Notation.js
@@ -0,0 +1,16 @@
+// // 02. JSON Syntax
+// let student = {
+//   jsonName: "Rahim",
+//   jsonAge: 26,
+//   jsonhometown: "dhaka",
+// };
+// //javascript to json convert
+// let student_json = JSON.stringify(student);
+
+// document.write(student_json);
+// //json to javascript convert
+// let student_new = JSON.parse(student_json);
+
+// console.log(student_new);
+
+// // 03. JSON Data Types
