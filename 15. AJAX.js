d7d3230d5ce3fd88bfdb45fed15fa0b359a6a622
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 20:06:10 +0600

    get jokes

--- 15. AJAX.js
+++ 15. AJAX.js
@@ -1,7 +1,47 @@
-// Asynchronous Programming
-// AJAX = Asynchronous Javascript and XML
-// Get Data without loading the page
-document.getElementById("get_data").addEventListener("click", loadData);
+// // Asynchronous Programming
+// // AJAX = Asynchronous Javascript and XML
+// // Get Data without loading the page
+// document.getElementById("get_data").addEventListener("click", loadData);
+
+// function loadData() {
+//   // create XHR Object (Xml Http Request)
+//   let ajxh = new XMLHttpRequest();
+
+//   // open function
+//   ajxh.open("GET", "data.txt", true);
+
+//   ajxh.onprogress = function () {
+//     console.log(ajxh.readyState);
+//   };
+
+//   //HTTP Status
+//   // 200: "OK"
+//   // 403: "Forbidden"
+//   // 404: "Not Found"
+
+//   ajxh.onload = function () {
+//     if (this.status === 200) {
+//       document.getElementById(
+//         "ajoutput"
+//       ).innerHTML = `<h4>${this.responseText}</h4>`;
+//     }
+//   };
+
+//   //Old Style
+//   // ajxh.onreadystatechange = function () {
+//   //   if (this.status === 200 && this.readyState === 4) {
+//   //     document.getElementById(
+//   //       "ajoutput"
+//   //     ).innerHTML = `<h4>${this.responseText}</h4>`;
+//   //   }
+//   // };
+
+//   ajxh.send();
+// }
+
+//02. Data From External Server
+
+document.getElementById("get_jokes").addEventListener("click", loadData);
 
 function loadData() {
   // create XHR Object (Xml Http Request)
@@ -10,15 +50,10 @@ function loadData() {
   // open function
   ajxh.open("GET", "data.txt", true);
 
-  //HTTP Status
-  // 200: "OK"
-  // 403: "Forbidden"
-  // 404: "Not Found"
-
   ajxh.onload = function () {
     if (this.status === 200) {
       document.getElementById(
-        "ajoutput"
+        "jokes"
       ).innerHTML = `<h4>${this.responseText}</h4>`;
     }
   };
