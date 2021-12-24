955067e61d2ed96876e583eb73e40eecf5760ab2
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 15:27:33 +0600

    Update 14. JavaScript Object Notation.js

--- 14. JavaScript Object Notation.js
+++ 14. JavaScript Object Notation.js
@@ -14,24 +14,26 @@
 // console.log(student_new);
 
 // // 03. JSON Data Types
-//string
-//number
-//object
-//array
-//boolean
-//null
+// //string
+// //number
+// //object
+// //array
+// //boolean
+// //null
 
-let jsonperson = {
-  name: "Borhan", //string
-  age: 25, //number
-  hometown: "Kushtia",
-  married: false, //boolean
-  dob: "1996-07-08", //date
-  test_null: null, //null
-  test_undefined: undefined, //undefined
-  greet: function () {
-    console.log(`Hello ${name}`);
-  }, // function
-};
-let jsonpersonc = JSON.stringify(jsonperson);
-console.log(jsonpersonc);
+// let jsonperson = {
+//   name: "Borhan", //string
+//   age: 25, //number
+//   hometown: "Kushtia",
+//   married: false, //boolean
+//   dob: "1996-07-08", //date
+//   test_null: null, //null
+//   test_undefined: undefined, //undefined
+//   greet: function () {
+//     console.log(`Hello ${name}`);
+//   }, // function
+// };
+// let jsonpersonc = JSON.stringify(jsonperson);
+// console.log(jsonpersonc);
+
+// 04. Warning!
