85f6db61d1343d490ca6d137aff522523294eec4
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 16:53:18 +0600

    comment out

--- 14. JavaScript Object Notation.js
+++ 14. JavaScript Object Notation.js
@@ -1,44 +1,44 @@
-// // 02. JSON Syntax
-// let student = {
-//   jsonName: "Rahim",
-//   jsonAge: 26,
-//   jsonhometown: "dhaka",
-// };
-// //javascript to json convert
-// let student_json = JSON.stringify(student);
+// 02. JSON Syntax
+let student = {
+  jsonName: "Rahim",
+  jsonAge: 26,
+  jsonhometown: "dhaka",
+};
+//javascript to json convert
+let student_json = JSON.stringify(student);
 
-// document.write(student_json);
-// //json to javascript convert
-// let student_new = JSON.parse(student_json);
+document.write(student_json);
+//json to javascript convert
+let student_new = JSON.parse(student_json);
 
-// console.log(student_new);
+console.log(student_new);
 
-// // 03. JSON Data Types
-// //string
-// //number
-// //object
-// //array
-// //boolean
-// //null
+// 03. JSON Data Types
+//string
+//number
+//object
+//array
+//boolean
+//null
 
-// let jsonperson = {
-//   name: "Borhan", //string
-//   age: 25, //number
-//   hometown: "Kushtia",
-//   married: false, //boolean
-//   dob: "1996-07-08", //date
-//   test_null: null, //null
-//   test_undefined: undefined, //undefined
-//   greet: function () {
-//     console.log(`Hello ${name}`);
-//   }, // function
-// };
-// let jsonpersonc = JSON.stringify(jsonperson);
-// console.log(jsonpersonc);
+let jsonperson = {
+  name: "Borhan", //string
+  age: 25, //number
+  hometown: "Kushtia",
+  married: false, //boolean
+  dob: "1996-07-08", //date
+  test_null: null, //null
+  test_undefined: undefined, //undefined
+  greet: function () {
+    console.log(`Hello ${name}`);
+  }, // function
+};
+let jsonpersonc = JSON.stringify(jsonperson);
+console.log(jsonpersonc);
 
-// // 04. Warning!
-//frist use this
-// let jsonpersonc = JSON.stringify(jsonperson);
+// 04. Warning!
+// frist use this
+let jsonpersonc = JSON.stringify(jsonperson);
 
 //05. JSON Objects and Array
 var xhttp = new XMLHttpRequest();
