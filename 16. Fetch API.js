8ee2af4c9248049d0edd751218317e9edfdf476c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-24 22:46:58 +0600

    update

--- 16. Fetch API.js
+++ 16. Fetch API.js
@@ -1,1 +1,26 @@
 //Callback Function
+let persons = [
+  { fristName: "Borhan", lastName: "Uddin" },
+  { fristName: "Rabby", lastName: "Biswas" },
+];
+
+function createPerson(person, getperson) {
+  setTimeout(function () {
+    persons.push(person);
+    getperson();
+  }, 2000);
+}
+
+function getPerson() {
+  setTimeout(function () {
+    let output = "";
+    persons.forEach(function (person) {
+      output += `<li>${person.fristName} ${person.lastName}</li>`;
+    });
+    document.getElementById("output").innerHTML = output;
+  }, 500);
+}
+
+createPerson({ fristName: "Rony", lastName: "Ali" }, getPerson);
+
+//02. JavaScript Promises
