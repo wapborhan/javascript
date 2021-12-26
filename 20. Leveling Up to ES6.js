f9cd0d8435258cd59ac95be6b0587c702d19ab6c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-26 20:47:09 +0600

    Update 20. Leveling Up to ES6.js

--- 20. Leveling Up to ES6.js
+++ 20. Leveling Up to ES6.js
@@ -1,9 +1,25 @@
 //ES6 fat arrow Function
+
 //Normal Function
 // function number() {
 //   return 10;
 // }
 
-let number = (a, b) => a + b;
-
+//Arrow Function
+let number = (a, b) => {
+  a + b;
+};
 console.log(number(10, 15));
+
+
+var javascript = {
+  name: "javascript",
+  libraries: ["react", "angular", "vue"],
+  printlibr = function () {
+    this.libraries.forEach((a) => {
+      console.log(`${this.name} loves ${a}`);
+    });
+  }
+};
+
+javascript.printlibr();
