23972194cc169015c437cd9d73b087f763464e2a
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-26 21:22:24 +0600

    Update 20. Leveling Up to ES6.js

--- 20. Leveling Up to ES6.js
+++ 20. Leveling Up to ES6.js
@@ -6,7 +6,23 @@
 // }
 
 //Arrow Function
-let number = (a, b) => {
-  a + b;
-};
-console.log(number(10, 15));
+// let number = (a, b) => {
+//   a + b;
+// };
+// console.log(number(10, 15));
+
+//Ternary Oparetor
+var age = 18;
+//short form
+var type = age >= 18 ? "Adult" : age < 10 ? "Child" : "young";
+// logn Form
+
+if (age >= 18) {
+  type = "adult";
+} else if (age <= 10) {
+  ("Child");
+} else {
+  type = "Young";
+}
+
+console.log(type);
